1be94dd51a00970907cbda6ddb1a11182c649c62
Sean Vreeland <seanvree@hotmail.com>
2018-03-18 05:05:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ $(document).ready(function () {
 				//FTP:
 				'm779126193-13d3b5b9a3840099036fdbb9',
 				//Headhpones:
-				'm779126189-61a8c36d7fca73658485127b,
+				'm779126189-61a8c36d7fca73658485127b',
 				//Jackett:
-				'm779126194-a44ef2b1d6a8ff64ae900b68'
+				'm779126194-a44ef2b1d6a8ff64ae900b68',
+				//LazyLibrarian
+				'm779368951-ecd6233582d5d84384aca4d6'
 			],
 			logs: 1,
 			response_times: 1,
